831e23a089965b47f95f5a76f2ef5d53e5aca424
Silas Eliaquim <silastoreto@gmail.com>
2021-03-25 19:49:57 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	
+	//essa classe cria os Daos
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	// essa classe expoe a interface mas instancia um objeto dentro dela 
+	// assim expomos so a interface ao inves de implementarmos o objeto
+	
+	// no programa principal não chamaremos Objeto obj = new Objet..
+	// sera chamadoa assim - Objeto obj = DaoFactory.createSellerDao
+}
